5ce39d0abe1af27f3f34878b8345366e99e0600c
Arfan A. Khan <73901113+ArfanGits@users.noreply.github.com>
2021-07-11 21:26:34 +0600

    Authentication

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,7 +1,9 @@
 import * as actionTypes from './actionTypes';
+import { navigate } from '../NavigationRoot';
 
-export const addPlace = place => dispatch => {
-    fetch("https://my-places-e91d8-default-rtdb.firebaseio.com/places.json", {
+export const addPlace = place => (dispatch, getState) => {
+    let token = getState().token;
+    fetch(`https://my-places-e91d8-default-rtdb.firebaseio.com/places.json?auth=${token}`, {
         method: "POST",
         body: JSON.stringify(place)
     })
@@ -17,8 +19,9 @@ export const setPlaces = places => {
     }
 }
 
-export const loadPlaces = () => dispatch => {
-    fetch("https://my-places-e91d8-default-rtdb.firebaseio.com/places.json")
+export const loadPlaces = () => (dispatch, getState) => {
+    let token = getState().token;
+    fetch(`https://my-places-e91d8-default-rtdb.firebaseio.com/places.json?auth=${token}`)
         .catch(err => {
             alert("Something went wrong, sorry");
             console.log(err);
@@ -42,4 +45,54 @@ export const deletePlace = key => {
         type: actionTypes.DELETE_PLACE,
         payload: key
     }
+}
+
+export const authUser = token => {
+    return {
+        type: actionTypes.AUTHENTICATE_USER,
+        payload: token
+    }
+}
+
+export const removeToken = () => {
+    return {
+        type: actionTypes.REMOVE_TOKEN
+    }
+}
+
+export const tryAuth = (email, password, mode) => dispatch => {
+    let url = "";
+    const API_KEY = "AIzaSyAUmVHibWnsK3J1srQpi7cfEZ33Wvqc3Kc";
+    if (mode === "signup") {
+        url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY;
+    } else if (mode === "login") {
+        url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + API_KEY;
+    }
+    console.log(url);
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .catch(err => {
+            console.log(err);
+            alert("Authentication Failed!");
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.error) {
+                alert(data.error.message);
+            } else {
+                dispatch(authUser(data.idToken));
+                navigate("Places");
+            }
+            console.log(data)
+        })
+
 }
--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -3,3 +3,6 @@ export const DELETE_PLACE = 'DELETE_PLACE';
 
 export const LOAD_PLACES = 'LOAD_PLACES';
 export const SET_PLACES = 'SET_PLACES';
+
+export const AUTHENTICATE_USER = 'AUTHENTICATE_USER';
+export const REMOVE_TOKEN = 'REMOVE_TOKEN';
